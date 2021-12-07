631afbb51e957466a188cd9668502bf387d03830
Minh <trongminhtbh@gmail.com>
2021-12-07 20:21:52 +0700

    change

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
-    introTitle: 'Hế luuuu màu Tím đỏ.',
-    introDesc: `Hôm nay, là ngày đặc biệt…
-    Tớ có điều muốn nói với cậu, cậu có muốn nghe hông?`,
+    introTitle: 'Hello Cleoyii.',
+    introDesc: `Today is your special day…
+    I want to say somethings nice to you`,
     btnIntro: 'OK',
-    title: 'Nay là sinh nhật tuổi 21 của cậu rồi ấy, đố cậu biết tớ đang nghĩ gì nào?',
-    desc: 'Ờ,  trả lời cho tớ biết đi.> ',
-    btnYes: 'Uhmm, để đoán thử. ',
-    btnNo: 'Không biết, đoán không ra. ',
-    question:'Chắc là',
-    reasonPlaceholder: 'Gõ vào đây nè...',
+    title: ':>',
+    desc: ' Do you want to know? ',
+    btnYes: 'let me guess ',
+    btnNo: 'Noo',
+    question:'Maybe',
+    reasonPlaceholder: 'type some thing ....',
     btnReply: 'Send',
-    reply: 'Nay 21 tuổi rồi ấy, sinh nhật thật vui vẻ nha, thật nhiều tiếng cười, thật hạnh phúc… Và sang tuổi mới, kiếm được nhiều tiền nè. Và đặc biệt… hãy luôn là người con gái thật rạng ngời trong cuộc sống nhé.',
-    mess: 'Đoán đúng rồi ấy, Cậu thấy chưa tụi mình hợp ý nhau ghê!',
-    messDesc: 'Đó, xong rồi đó.... Sinh nhật zui zẻ nhaaa. Tím đỏ của tớ.',
-    btnAccept: 'Ok lun',
+    reply: 'Happy birthday to you!',
+    mess: '',
+    messDesc: 'Done',
+    btnAccept: 'Ok',
 }
